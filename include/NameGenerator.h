42fbce66b098c9a8c96378e03765a59698de3ba3
nogalevy <nogalevy2@gmail.com>
2022-04-10 22:23:42 +0300

    print sets name in circular way

--- include/NameGenerator.h
+++ include/NameGenerator.h
@@ -3,17 +3,19 @@
 #include <utility>
 
 // Generates set names, A-Z
+const char FIRST = 'A';
 class NameGenerator
 {
 public:
     // Generates the next name
     char generate()
     {
+        m_current = ((m_current - FIRST) % 26) + FIRST;
         return m_current++;
     }
 
 private:
-    char m_current = 'A';
+    char m_current = FIRST;
 };
 
 //TODO: make better
